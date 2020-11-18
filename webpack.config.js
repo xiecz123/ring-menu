b226b30dd17f20320187b04f27d7ff77069abd3a
xiecz123 <702421862@qq.com>
2020-11-18 17:42:01 +0800

    完成webpack打包

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require('path')
+const webpack = require('webpack')
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const isDev = process.env.NODE_ENV === 'development'
+
+function resolve (src) {
+  return path.resolve(__dirname, src)
+}
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[hash:6].bundle.js',
+    path: resolve('dist')
+  },
+  mode: isDev ? 'development' : 'production',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['**/*', '!dll', '!dll/**'] // 不删除dll目录下的文件
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    port: '3000',
+    stats: 'errors-only',
+    hot: true,
+    proxy: 'http://localhost:4000'
+  },
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      // {
+      //   test: /\.js$/,
+      //   use: ['babel-loader'],
+      //   exclude: /node_modules/
+      // },
+      {
+        test: /\.(le|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('autoprefixer')({
+                    overrideBrowserslist: [
+                      '>0.25%',
+                      'not dead'
+                    ]
+                  })
+                ]
+              }
+            }
+          }, 'less-loader'
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg|webp|svg|eot|ttf|woff|woff2)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 10240, // 10K
+            esModule: false,
+            name: '[name]_[hash:6].[ext]',
+            outputPath: 'assets'
+          }
+        }],
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
